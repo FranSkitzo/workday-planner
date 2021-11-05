214a1d537d03c636e9481e0d36d88e68b19d2d9f
Rhogar <johnfmartinez597@gmail.com>
2021-11-05 13:37:57 -0400

    fixed the functionality of color changing the containers based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
-var currentTime = parseInt(moment().format('LL'));
+var currentTime = parseInt(moment().format('H'));
 var classes = [".8AM", ".9AM", ".10AM", ".11AM", ".12PM", ".1PM", ".2PM", ".3PM", ".4PM", "5PM", ".6PM"]
 var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 var classIndex = time.indexOf(currentTime);
@@ -37,7 +37,7 @@ function allFuture() {
     for (i = 1; i < classes.length; i++) {
         $(classes[i]).addClass("future");
     }
-    $(classes[i]).addClass("present");
+    $(classes[0]).addClass("present");
 }
 
 function allPast() {
